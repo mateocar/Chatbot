a8091a90a4eafa92538687bf7b5449820a914b8c
Mateo Cardenas <mateocar96@gmail.com>
2025-05-27 22:59:43 -0500

    ADD: se agregan modelos y squemas

---
+++ app/model.py
@@ -0,0 +1,17 @@
+import json
+import random
+import joblib
+
+model = joblib.load("app/chat_model.pkl")
+
+with open("app/intens.py", "r") as intt:
+    intents = json.load(intt)
+
+def get_response(user_input: str) -> str:
+    tag = model.predict([user_input])[0]
+
+    for intent in intents["intents"]:
+        if intent["tag"] == tag:
+            return random.choice(intent["responses"])
+    
+    return "Lo siento no entendi lo copiaste"
