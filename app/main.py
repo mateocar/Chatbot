0b3b672053b12e32aea2847f0bab78e1b636e5a4
Mateo Cardenas <mateocar96@gmail.com>
2025-06-12 20:16:56 -0500

    UPDATE: se agregan los corps y la configuracion en css

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from app.schemas import MessageRequest, MessageResponse
 from app.model import get_response
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/chatbot",response_model=MessageResponse)
 def get_responses(message: MessageRequest):
     response = get_response(message.text)
     
     return {"text": response}
+
 
 
